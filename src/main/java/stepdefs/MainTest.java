c8134e3c4aa60e2e011dc92317f211e0e60de68b
Daria Mikhalik <daria.mihalik@moneyman.ru>
2020-08-04 15:50:41 +0300

    Selenium Cucumber (changes2)

--- src/main/java/stepdefs/MainTest.java
+++ src/main/java/stepdefs/MainTest.java
@@ -5,9 +5,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @Cucumber.Options(features = {"src/main/resources"},
-        format = {"prety", "html:target/reports/cucumber/html",
-                "json;target/cucumber.json", "" + "usage:target/usage.jsonx", "junit:target/junit.xml"},
-        tags = {"@done}"})
+        format = {"prety", "html:target/reports/cucumber/html", "json;target/cucumber.json",
+                "" + "usage:target/usage.jsonx", "junit:target/junit.xml"}, tags = {"@done}"})
 
 public class MainTest {
 }
